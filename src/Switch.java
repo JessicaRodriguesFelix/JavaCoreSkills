e3808cb1b27a299416658631e7fd1b804623981b
JessicaRodriguesFelix <jessicarodrigues_felix@hotmail.com>
2023-01-26 16:12:28 -0500

    exercises section 6

--- src/Switch.java
+++ src/Switch.java
@@ -1,16 +1,23 @@
 public class Switch {
     public static void main(String[] args) {
-        int switchValue = 2;
-        switch (switchValue) {
-            case 1:
-                System.out.println("Value was 1");
-                break;
-            case 2:
-                System.out.println("Value was 2");
-                break;
-            default:
-                System.out.println("Value was not 1 or 2");
-                break;
-        }
+//        int switchValue = 2;
+//        switch (switchValue) {
+//            case 1 -> System.out.println("Value was 1");
+//            case 2 -> System.out.println("Value was 2");
+//            default -> System.out.println("Value was not 1 or 2");
+//        }
+        System.out.println(getQuarter("SEPTEMBER"));
+    }
+
+    public static String getQuarter(String month) {
+        return switch (month) {
+            case "JANUARY", "FEBRUARY", "MARCH" -> { yield "1st"; }
+            case "APRIL", "MAY", "JUNE" -> "2st";
+            case "JULY", "AUGUST", "SEPTEMBER" -> "3st";
+            default -> {
+                String badResponse = month + " is bad";
+                yield badResponse;
+            }
+        };
     }
 }
